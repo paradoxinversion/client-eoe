118bec6c54f12b60b39f248a9ebd0e7804697ea6
Jedai Saboteur <paradox.inversion@gmail.com>
2023-02-11 20:10:59 -0800

    Updated screens
    Signed-off-by: Jedai Saboteur <paradox.inversion@gmail.com>

--- src/screens/Personnel.jsx
+++ src/screens/Personnel.jsx
@@ -1,9 +1,37 @@
-const PersonnelScreen = () => {
+import PersonPanel from "../elements/PersonPanel";
+import { toDataArray } from "../utilities/dataHelpers";
+const eoe = require('empire-of-evil')
+const PersonnelScreen = ({gameData}) => {
   return (
     <section>
-      <header>
-        <p>Personnel</p>
+      <header className="text-3xl font-bold">
+        <p>Empire Personnel</p>
       </header>
+      <p>Empire Roster Total: {eoe.organizations.getAgents(
+          toDataArray(gameData.people),
+          gameData.player.organizationId
+        ).length}</p>
+      <PersonPanel 
+        title={"Troopers"}
+        people={eoe.organizations.getAgents(
+          toDataArray(gameData.people),
+          gameData.player.organizationId
+        ).filter(person => person.agent.department === 0)} 
+      />
+      <PersonPanel 
+        title={"Administrators"}
+        people={eoe.organizations.getAgents(
+          toDataArray(gameData.people),
+          gameData.player.organizationId
+        ).filter(person => person.agent.department === 1)} 
+      />
+      <PersonPanel 
+        title={"Scientists"}
+        people={eoe.organizations.getAgents(
+          toDataArray(gameData.people),
+          gameData.player.organizationId
+        ).filter(person => person.agent.department === 2)} 
+      />
     </section>
   );
 };
--- src/screens/WorldScreen.jsx
+++ src/screens/WorldScreen.jsx
@@ -1,9 +1,61 @@
-const WorldScreen = () => {
+import { useState } from "react";
+import ZonePanel from "../elements/ZonePanel";
+import { toDataArray } from "../utilities/dataHelpers";
+const eoe = require("empire-of-evil");
+const WorldScreen = ({ gameData }) => {
+  const [selectedNation, setSelectedNation] = useState(null);
   return (
     <section>
       <header>
-        <p>World</p>
+        <p className="text-3xl font-bold">World</p>
       </header>
+      <div className="flex">
+        <section>
+          <header className="text-2xl font-bold mb-2">
+            <p>Nations</p>
+          </header>
+          {toDataArray(gameData.nations)
+            .filter((nation) => nation.id !== gameData.player.empireId)
+            .map((nation) => (
+              <div>
+                <button
+                  className="mb-2 px-2 border rounded"
+                  onClick={() => {
+                    setSelectedNation(nation);
+                  }}
+                >
+                  {nation.name}
+                </button>
+              </div>
+            ))}
+        </section>
+        
+        {selectedNation && (
+          <section className="ml-4">
+            <header className="text-2xl font-bold mb-2">
+              <p>{selectedNation.name}</p>
+            </header>
+            <section className="mb-4">
+              <p>Nation Size: {selectedNation.size}?</p>
+              <p>
+                Nation Citizens:{" "}
+                {eoe.citizens.getCitizens(
+                  toDataArray(gameData.people),
+                  selectedNation.id
+                ).length}?
+              </p>
+              <p>Nation Agents: 9999?</p>
+            </section>
+            <ZonePanel
+              title={`${eoe.zones.getZones(toDataArray(gameData.zones), selectedNation.id).length} Zones`}
+              zones={eoe.zones.getZones(
+                toDataArray(gameData.zones),
+                selectedNation.id
+              )}
+            />
+          </section>
+        )}
+      </div>
     </section>
   );
 };
