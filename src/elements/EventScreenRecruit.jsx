d00ff7424ca9060de1f64d1b6772cffe6afa35be
Jedai Saboteur <paradox.inversion@gmail.com>
2023-02-18 16:22:17 -0800

    Updated event handling
    Signed-off-by: Jedai Saboteur <paradox.inversion@gmail.com>

---
+++ src/elements/EventScreenRecruit.jsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import Button from "./Button";
+
+/**
+ * @param {object} props
+ * @param {object} props.currentGameEvent 
+ * @param {Function} props.resolveEvent
+ * @returns 
+ */
+const EventScreenRecruit = ({currentGameEvent, resolveEvent}) => {
+  const [department, setDepartment] = useState(null);
+
+  const onChange = (event) => {
+    setDepartment(event.target.value);
+  }
+
+  return (
+    <section>
+      <section className="mb-4">
+        <header>
+          <h2>Recruit Details</h2>
+        </header>
+        <p>{currentGameEvent.recruit.name}</p>
+        <p>Combat: {currentGameEvent.recruit.combat}</p>
+        <p>Administration: {currentGameEvent.recruit.administration}</p>
+        <p>Intelligence: {currentGameEvent.recruit.intelligence}</p>
+      </section>
+
+      <section className="mb-4">
+        <p>Select a department for this recruit</p>
+        <form onChange={onChange}>
+          <div>
+            <input type="radio" id="recruit-department-trooper" name="recruit-department" value={0}/>
+            <label htmlFor="recruit-department-trooper">Henchman</label>
+          </div>
+          <div>
+            <input type="radio" id="recruit-department-administrator" name="recruit-department" value={1}/>
+            <label htmlFor="recruit-department-administrator">Administrator</label>  
+          </div>
+          <div>
+            <input type="radio" id="recruit-department-scientist" name="recruit-department" value={2}/>
+            <label htmlFor="recruit-department-scientist">Scientist</label>  
+          </div>
+        </form>
+      </section>
+      <section className="w-32 flex justify-between">
+        <Button
+          disabled={department === null}
+          buttonText="Accept"
+          onClick={()=>{
+            resolveEvent({
+              resolutionValue: 1,
+              data: {
+                department
+              }
+            })
+          }}
+        />
+
+        <Button 
+          buttonText="Deny"
+          onClick={()=>{
+            resolveEvent({
+              resolutionValue: 0
+            })
+          }}
+        />
+      </section>
+    </section>
+  )
+}
+
+export default EventScreenRecruit;
--- src/screens/EventScreen.jsx
+++ src/screens/EventScreen.jsx
@@ -1,20 +1,41 @@
 import { useState } from "react";
-
-const EventsScreen = ({ gameData, setScreen }) => {
+import EventScreenRecruit from "../elements/EventScreenRecruit";
+const eventScreenMap = {
+  "EVIL Applicant": EventScreenRecruit
+}
+const EventsScreen = ({ gameData, setGameData, setScreen }) => {
   const [eventIndex, setEventIndex] = useState(0);
+  const [eventScreen, setEventScreen] = useState(gameData.events[0].eventName)
+  const CurrentEventComponent = eventScreenMap[eventScreen];
+
+  const resolveEvent = (resolveArgs) => {
+    const resolution = gameData.events[eventIndex].resolveEvent(gameData, resolveArgs)
+    setGameData(resolution.updatedGameData);
+    if (eventIndex === gameData.events.length -1){
+      setScreen("main")
+    } else{
+      setEventIndex(eventIndex++)
+    }
+  }
+
   return (
     <section>
-      <header>
-        <p>{gameData.events[eventIndex].eventName}</p>
+      <header className="mb-4">
+        <h1>{gameData.events[eventIndex].eventName}</h1>
         <p>{gameData.events[eventIndex].eventText}</p>
-        <button onClick={()=>{
-          if (eventIndex === gameData.events.length -1){
-            setScreen("main")
-          } else{
-            setEventIndex(eventIndex++)
-          }
-        }}>Okay</button>
       </header>
+      <CurrentEventComponent
+        currentGameEvent={gameData.events[eventIndex]}
+        resolveEvent={resolveEvent}
+      />
+      {/* <button onClick={()=>{
+        if (eventIndex === gameData.events.length -1){
+          setScreen("main")
+        } else{
+          setEventIndex(eventIndex++)
+        }
+      }}>Okay</button> */}
+
     </section>
   );
 };
