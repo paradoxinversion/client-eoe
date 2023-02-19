5b5eee6d4cc4bb080ed7059f45065e299d7fcc73
Jedai Saboteur <paradox.inversion@gmail.com>
2023-02-18 16:22:30 -0800

    Updated advance day
    Signed-off-by: Jedai Saboteur <paradox.inversion@gmail.com>

--- src/actions/advanceDay.js
+++ src/actions/advanceDay.js
@@ -2,25 +2,36 @@ import { toDataArray } from "../utilities/dataHelpers";
 
 const eoe = require("empire-of-evil");
 
+/**
+ * Determines what events happen at end of turn and returns
+ * updated gamedata with those events.
+ * @param {*} gameData 
+ * @returns {object}
+ */
 const advanceDay = (gameData) => {
+  // SELECT EVENT
   const recruit = eoe.citizens
     .getCitizens(toDataArray(gameData.people), gameData.player.empireId)
     .filter((person) => !person.agent)[0];
-  const TEST_RECRUIT_EVENT = new eoe.gameEvents.RecruitEvent(
+  const TEST_RECRUIT_EVENT = new eoe.gameEvents.EvilApplicantEvent(
     recruit,
     gameData.player.organizationId
   );
+  // END SELECT EVENT
 
   TEST_RECRUIT_EVENT.executeEvent();
+
+  // Make a copy of the game data
   const updatedGameData = JSON.parse(JSON.stringify(gameData));
-  const updatedRecruit = {
-    ...recruit,
-    agent: TEST_RECRUIT_EVENT.eventData,
-  };
-  updatedGameData.people[recruit.id] = updatedRecruit;
+  // const updatedRecruit = {
+  //   ...recruit,
+  //   agent: TEST_RECRUIT_EVENT.eventData,
+  // };
+  // updatedGameData.people[recruit.id] = updatedRecruit;
   updatedGameData.events = [TEST_RECRUIT_EVENT];
-  console.log("Advance Day", TEST_RECRUIT_EVENT);
-  return { events: [TEST_RECRUIT_EVENT], updatedGameData };
+  
+  console.log(updatedGameData);
+  return { updatedGameData };
 };
 
 export default advanceDay;
