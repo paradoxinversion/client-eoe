eebb07553694d560bac845ec51cad03aec64dea3
Jedai Saboteur <paradox.inversion@gmail.com>
2023-03-10 01:20:22 -0800

    Add shufflebag
    Signed-off-by: Jedai Saboteur <paradox.inversion@gmail.com>

---
+++ src/utilities/shufflebag.js
@@ -0,0 +1,32 @@
+/**
+ *
+ * @param {*} frequencyMap - a key/value map of options and their frequencies
+ */
+export const Shufflebag = frequencyMap => {
+  const getValueSet = frequencyMap => {
+    let valueSet = [];
+    for (let entry in frequencyMap) {
+      for (let y = 0; y < frequencyMap[entry]; y++) {
+        valueSet.push(entry);
+      }
+    }
+    return valueSet;
+  };
+
+  const fmap = frequencyMap;
+  let values = getValueSet(fmap);
+  console.log("Shufflebag initialized:", fmap)
+  return {
+    /**
+     * Get the next value in the shufflebag.
+     */
+    next() {
+      const selectedValue = Math.floor(Math.random() * values.length);
+      const selection = values[selectedValue];
+      values.splice(selectedValue, 1);
+      if (values.length === 0) values = getValueSet(fmap);
+      console.log(values.length);
+      return selection;
+    }
+  };
+};
