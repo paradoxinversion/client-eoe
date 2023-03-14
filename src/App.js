d46970621a55690b335e7e018e98f851de66435b
Jedai Saboteur <paradox.inversion@gmail.com>
2023-03-13 21:27:53 -0700

    Add victory screen, update gameData logic

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import EventsScreen from "./screens/EventScreen";
 import { GameEventQueue } from "empire-of-evil/src/gameEvents";
 import { ActivityManager, PlotManager } from "empire-of-evil/src/plots";
 import GameOverScreen from "./screens/GameOverScreen";
+import VictoryScreen from "./screens/VictoryScreen";
 
 const screens = {
   title: TitleScreen,
@@ -24,23 +25,46 @@ const screens = {
   plots: PlotsScreen,
   world: WorldScreen,
   events: EventsScreen,
-  "game-over": GameOverScreen
+  "game-over": GameOverScreen,
+  victory: VictoryScreen
 };
 
 function App() {
   const [gameData, _setGameData] = useState({});
   const [eventQueue] = useState(new GameEventQueue());
-  const [activityManager] = useState(new ActivityManager())
-  const [plotManager] = useState(new PlotManager())
+  const [activityManager] = useState(new ActivityManager());
+  const [plotManager] = useState(new PlotManager());
   const [gameScreen, setGameScreen] = useState("title");
   const CurrentScreen = screens[gameScreen];
+
+  /**
+   * 
+   * @param {import("empire-of-evil/src/typedef").GameData} updatedGameData 
+   */
+  const updateGameData = (updatedGameData) => {
+    _setGameData({
+      ...gameData,
+      people: { ...gameData.people, ...updatedGameData.people },
+      zones: { ...gameData.zones,...updatedGameData.zones},
+      nations: { ...gameData.nations,...updatedGameData.nations},
+      governingOrganizations: { ...gameData.governingOrganizations,...updatedGameData.governingOrganizations},
+      buildings: { ...gameData.buildings,...updatedGameData.buildings},
+      player: { ...gameData.player, ...updatedGameData.player},
+      gameDate: updatedGameData.gameDate || gameData.gameDate
+    });
+  };
   return (
     <div className="bg-stone-900 text-stone-200 h-screen grid grid-cols-12 gap-4">
       {Object.keys(gameData).length > 0 && (
-        <ScreenNavigator gameData={gameData} setScreen={setGameScreen} />
+        <ScreenNavigator gameData={gameData} setScreen={setGameScreen} activityManager={activityManager} />
       )}
-      <div className={`overflow-y-auto w-full ${Object.keys(gameData).length > 0 ? "col-span-11" : "col-span-12"}`}>
+      <div
+        className={`overflow-y-auto w-full ${
+          Object.keys(gameData).length > 0 ? "col-span-11" : "col-span-12"
+        }`}
+      >
         <CurrentScreen
+          updateGameData={updateGameData}
           setScreen={setGameScreen}
           gameData={gameData}
           setGameData={_setGameData}
