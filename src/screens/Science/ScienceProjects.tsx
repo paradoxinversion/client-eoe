25559d1cffb72aa0e561fba4278bff10671acf6b
Jedai Saboteur <paradox.inversion@gmail.com>
2024-03-16 13:13:40 -0700

    cleanup
    Signed-off-by: Jedai Saboteur <paradox.inversion@gmail.com>

--- src/screens/Science/ScienceProjects.tsx
+++ src/screens/Science/ScienceProjects.tsx
@@ -18,76 +18,56 @@ import { setProjects } from "../../features/scienceSlice";
 interface ScienceProjectProps {
   gameManager: GameManager;
 }
-const ScienceProjects = ({gameManager}: ScienceProjectProps) => {
+const ScienceProjects = ({ gameManager }: ScienceProjectProps) => {
   const dispatch = useAppDispatch();
-  const currentProjects = useAppSelector((state) => state.science.activeProjects);
+  const currentProjects = useAppSelector(
+    (state) => state.science.activeProjects
+  );
   return (
     <>
       <Grid container spacing="1rem" padding="1rem" columns={9}>
-        {
-          Object.values(gameManager.scienceManager.PROJECTS).filter(project => {
-            return !currentProjects.find(ap => ap.indexName === project.indexName);
-          }).map((project) => {
+        {Object.values(gameManager.scienceManager.PROJECTS)
+          .filter((project) => {
+            return !currentProjects.find(
+              (ap) => ap.indexName === project.indexName
+            );
+          })
+          .map((project) => {
             return (
               <Grid item xs={3} key={project.name}>
                 <Card variant="outlined">
                   <CardHeader title={project.name} />
                   <Divider />
                   <CardContent>
-                    <Stack paddingBottom={'0.5rem'} direction="row">
+                    <Stack paddingBottom={"0.5rem"} direction="row">
                       <HourglassFullIcon />
                       <Typography>{project.completionTime}</Typography>
                     </Stack>
                     <Divider />
-                    <Box sx={{maxHeight: '250px', overflow: 'scroll'}}>
-                      <Typography>
-                        {project.description}
-                      </Typography>
+                    <Box sx={{ maxHeight: "250px", overflow: "scroll" }}>
+                      <Typography>{project.description}</Typography>
                     </Box>
                   </CardContent>
                   <Divider />
                   <CardActionArea>
-                    <Button onClick={()=>{
-                      gameManager.scienceManager.startProject(gameManager, project.indexName);
-                      dispatch(setProjects(gameManager.scienceManager.activeProjects));
-                    }}>Start Research</Button>
+                    <Button
+                      onClick={() => {
+                        gameManager.scienceManager.startProject(
+                          gameManager,
+                          project.indexName
+                        );
+                        dispatch(
+                          setProjects(gameManager.scienceManager.activeProjects)
+                        );
+                      }}
+                    >
+                      Start Research
+                    </Button>
                   </CardActionArea>
                 </Card>
               </Grid>
             );
-          })
-        }
-        <Grid item xs={3}>
-          <Card variant="outlined">
-            <CardHeader title="Increase Gigawatts" />
-            <Divider />
-            <CardContent >
-              <Stack paddingBottom={'0.5rem'} direction="row">
-                <HourglassFullIcon />
-                <Typography>2 Months</Typography>
-              </Stack>
-              <Divider />
-              <Box sx={{maxHeight: '250px', overflow: 'scroll'}}>
-
-                <Typography>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
-                  pulvinar ligula bibendum, mattis elit in, luctus eros. Nulla
-                  at gravida nibh, bibendum convallis justo. Sed auctor
-                  sollicitudin elit, a tincidunt nisi pulvinar at. Nullam sed
-                  velit massa. Aenean interdum leo risus, id cursus mauris
-                  gravida id. Ut odio erat, blandit quis vulputate at, aliquet a
-                  nibh. Vivamus a lorem sodales, ultricies libero sed, aliquet
-                  augue. Donec varius molestie turpis, vel laoreet purus
-                  fermentum vel.
-                </Typography>
-              </Box>
-            </CardContent>
-            <Divider />
-            <CardActionArea>
-              <Button>Start Research</Button>
-            </CardActionArea>
-          </Card>
-        </Grid>
+          })}
       </Grid>
     </>
   );
