90365ec385bbc4c9232acc8dd993576217fffd52
Jedai Saboteur <paradox.inversion@gmail.com>
2024-03-16 14:20:21 -0700

    Update attack event and gameover screen
    Signed-off-by: Jedai Saboteur <paradox.inversion@gmail.com>

--- src/elements/EventScreens/EventScreenCombatResults.tsx
+++ src/elements/EventScreens/EventScreenCombatResults.tsx
@@ -1,29 +1,68 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Divider, Typography } from "@mui/material";
 import { GameEventComponentProps } from "../../screens/Events/EventScreen";
+
+const combatRoundSeconds = 3;
 
 const EventScreenCombatResults = ({
   currentGameEvent,
   resolveEvent,
 }: GameEventComponentProps) => {
+  console.log(currentGameEvent.params.attackZone);
+  const wasVictorious =
+    currentGameEvent.params.attackZone.plot.resolution.data.victoryResult;
+  const rounds = currentGameEvent.params.attackZone.plot.resolution.data.rounds;
+
+  const getCombatTime = () => {
+    const combatSeconds = rounds * combatRoundSeconds;
+    if (combatSeconds < 60) {
+      return { value: combatSeconds, unit: "seconds" };
+    } else if (combatSeconds < 3600) {
+      return { value: combatSeconds / 60, unit: "minutes" };
+    } else {
+      return { value: combatSeconds / 3600, unit: "hours" };
+    }
+  };
+
+  const combatTime = getCombatTime();
   return (
     <Box>
-      <Box component="section">
-        <Typography>Combat Log</Typography>
+      <Box id="title-header" component="section">
+        <Typography id="title" variant="h4">
+          After Action Report
+        </Typography>
       </Box>
-      <Box>
+      <Divider />
+      <Box
+        id="combat-log-container"
+        sx={{ height: "15rem", overflowY: "scroll" }}
+      >
         {currentGameEvent?.params?.attackZone.plot?.resolution?.data?.combatLog?.map(
           (logString, index) => (
-            <Typography key={`log-${index}`}>{logString}</Typography>
+            <Typography key={`log-${index}`} variant="body2">
+              {logString}
+            </Typography>
           )
         )}
       </Box>
-      <Button
-        onClick={() => {
-          resolveEvent();
-        }}
-      >
-        Okay
-      </Button>
+      <Divider />
+      {wasVictorious ? (
+        <Typography>
+          You were victorious in {`${combatTime.value}`} {`${combatTime.unit}`}!
+        </Typography>
+      ) : (
+        <Typography>
+          You were defeated in {`${combatTime.value}`} {`${combatTime.unit}`}!
+        </Typography>
+      )}
+      <Box id="commands">
+        <Button
+          onClick={() => {
+            resolveEvent();
+          }}
+        >
+          Okay
+        </Button>
+      </Box>
     </Box>
   );
 };
--- src/screens/Endgame/GameOverScreen.jsx
+++ src/screens/Endgame/GameOverScreen.jsx
@@ -1,18 +1,35 @@
-import { Box, Button, Card, CardContent, Typography, Toolbar } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Divider,
+  Typography,
+  Toolbar,
+} from "@mui/material";
 
 const GameOverScreen = () => {
   return (
     <Box>
-      <Toolbar />
-      <Box>
-        <Typography>Game Over</Typography>
+      <Box padding="1rem">
+        <Typography variant="h4">Game Over</Typography>
       </Box>
-      <Typography>
-        The EVIL Empire has fallen. You had one job: to take over the world. You
-        failed. Your shame will be felt for generations in the zones you sullied
-        with your failed attempt at glory. You should feel bad.
-      </Typography>
-      <Typography>You can try again by reloading the page.</Typography>
+      <Divider />
+      <Box padding="1rem">
+        <Typography>
+          The EVIL Empire has fallen. You had one job: to take over the world.
+          You failed. Your shame will be felt for generations in the zones you
+          sullied with your failed attempt at glory. You should feel bad.
+        </Typography>
+        <Typography>You can try again by reloading the page.</Typography>
+        <Button
+          onClick={() => {
+            window.location.reload();
+          }}
+        >
+          Try Again
+        </Button>
+      </Box>
     </Box>
   );
 };
