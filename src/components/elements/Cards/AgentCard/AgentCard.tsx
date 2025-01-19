7cbd00bc7c63313e1551e94bba8ab9bcd58bbd3d
Jedai Saboteur <paradox.inversion@gmail.com>
2025-01-19 12:51:15 -0700

    refactor: fix managers ref

--- src/components/elements/Cards/AgentCard/AgentCard.tsx
+++ src/components/elements/Cards/AgentCard/AgentCard.tsx
@@ -9,7 +9,7 @@ import {
   Typography,
 } from "@mui/material";
 import { LocationCity as LocationCityIcon } from "@mui/icons-material";
-import managers from "empire-of-evil";
+import { managers } from "empire-of-evil";
 import { Person } from "empire-of-evil/types";
 import { selectEntity } from "../../../../features/selectionSlice";
 import IconContent from "../../IconInt/IconInt";
